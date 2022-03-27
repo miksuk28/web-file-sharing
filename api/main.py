cc715e3fde9e62b06c7c53ca34f39a6a36e3b7d0
miksuk28 <miksuk28@outlook.com>
2022-03-27 12:36:05 +0200

    cleanly exits

--- api/main.py
+++ api/main.py
@@ -1,12 +1,12 @@
 import os
 from flask import Flask, jsonify, abort, render_template, request
 from werkzeug.utils import secure_filename
+import sys, signal
 import db
 
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "/home/mikhail/Programming/Python/web-file-sharing/uploads"
-db = db.FilesWrapper("files.db")
 
 
 @app.route("/api/upload", methods=["POST", "GET"])
@@ -24,6 +24,8 @@ def upload():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
 
+            db.add_file(filename)
+
             return jsonify({
                 "status": "OK - File uploaded",
                 "filename": filename
@@ -40,5 +42,14 @@ def get_upload():
     return jsonify({"message": "OK"})
 
 
+def exit_handler(signal, frame):
+    db.close_db()
+    print("CTRL-C Pressed - QUITTING")
+    sys.exit(0)
+
 if __name__ == "__main__":
+    # Cleanly exit on CTRL-C
+    signal.signal(signal.SIGINT, exit_handler)
+    
+    db = db.FilesWrapper("files.db")
     app.run(host="127.0.0.1", port=5050)
